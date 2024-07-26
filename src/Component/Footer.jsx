09d4ddb2285df4a591788979c1cb096e386bc3df
MajdTamimi <majd2004tamimi@gmail.com>
2024-07-26 22:28:51 +0300

    using map

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,64 +1,57 @@
 const footer = () => {
-  
+  const languages = [
+    "English (US)", "العربية", "Français (France)", "Deutsch", "Русский",
+    "עברית", "Bahasa Indonesia", "Español", "Português (Brasil)", "Italiano", "हिन्दी"
+  ]
+  const links = [
+    "Sign Up", "Log In", "Messenger", "Facebook Lite", "Video", "Places", "Games",
+    "Marketplace", "Meta Pay", "Meta Store", "Meta Quest", "Meta AI", "Instagram",
+    "Threads", "Fundraisers", "Services", "Voting Information Center", "Privacy Policy",
+    "Privacy Center", "Groups", "About", "Create ad", "Create Page", "Developers",
+    "Careers", "Cookies", "Ad choices", "Terms", "Help", "Contact Uploading & Non-UsersSettings"
+  ]
+
   return (
     <footer className="text-[#1c1e21] pl-10 lg:text-start pt-8 lg:pt-7">
       <div className="max-w-[980px] mx-auto w-[1513px] h-[157px]">
         <div className="text-[12px] font-inherit w-[684px] lg:w-[980px] h-[28px]">
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">English (US)</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">العربية</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Français (France)</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Deutsch</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Русский</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">עברית</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Bahasa Indonesia</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Español</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Português (Brasil)</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">Italiano</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline">हिन्दी</a>
-          <a role="button" className="inline-block cursor-pointer list-none border border-[#ccd0d5] rounded-[2px] box-border mx-2 font-bold antialiased justify-center px-[8px] text-center text-[#8a8d91] bg-[#f5f6f7] duration-200 hover:bg-[#ebecec]" rel="dialog" href="#" title="Show more languages">
+          {
+            languages.map((language, index) => (
+              <a key={index} href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-2">
+                {language}
+              </a>
+            ))
+          }
+          <a role="button" className="inline-block cursor-pointer list-none border border-[#ccd0d5] rounded-[2px] box-border font-bold antialiased justify-center px-[8px] text-center text-[#8a8d91] bg-[#f5f6f7] duration-200 hover:bg-[#ebecec]" rel="dialog" href="#" title="Show more languages">
             <i className="inline-block cursor-pointer antialiased font-bold text-center leading-[18px] text-[#8a8d91] whitespace-nowrap relative bottom-[1px] align-middle icon-1"></i>
           </a>
         </div>
         <hr className="pb-2 lg:mt-1 mt-4" />
         <div className="text-[12px] font-inherit w-[650px] lg:w-[950px] h-[58px] flex flex-wrap">
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Sign Up</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Log In</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Messenger</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Facebook Lite</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Video</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Places</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Games</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Marketplace</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Meta Pay</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Meta Store</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Meta Quest</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Meta AI</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Instagram</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Threads</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Fundraisers</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Services</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Voting Information Center</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Privacy Policy</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Privacy Center</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Grups</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">About</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Create ad</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Create Page</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Developers</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Careers</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Cookies</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline">Ad choices</a>
-          <a href="#" className="inline-block text-[12px] leading-[1.6] cursor-pointer whitespace-nowrap text-[#8a8d91] relative top-[1px] align-middle mr-4 icon-2"></a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Terms</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Help</a>
-          <a href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">Contact Uploading & Non-UsersSettings</a>
+          {links.map((link, index) => {
+            if (index === 26) {
+              return (
+                <>
+                  <a key={index} href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline">
+                    {link}
+                  </a>
+                  <a href="#" className="inline-block text-[12px] leading-[1.6] cursor-pointer whitespace-nowrap text-[#8a8d91] relative top-[1px] align-middle mr-4 icon-2"></a>
+                </>
+              )
+            }
+            return (
+              <a key={index} href="#" className="text-[#8A8d91] font-custom text-[12px] font-inherit hover:underline mr-4">
+                {link}
+              </a>
+            )
+          })}
         </div>
         <br />
         <div className="text-[#8A8d91] font-custom text-[12px] font-inherit">
           <p>Meta &copy; 2024</p>
         </div>
       </div>
-    </footer>
+    </footer >
   )
 }
 
