9d4d96984d5c6325ec73198cf6c780d5c0364ee8
MajdTamimi <majd2004tamimi@gmail.com>
2024-08-03 21:28:08 +0300

    make width full

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ const login = () => {
   return (
     <>
       <div>
-        <div className="bg-[#f0f2f5] flex flex-col lg:flex-row min-w-[500px] pb-[112px] pt-8 lg:pt-[72px] lg:w-[1512px] lg:h-[710px] justify-center lg:gap-20 lg:items-start items-center">
+        <div className="bg-[#f0f2f5] flex flex-col lg:flex-row min-w-full pb-[112px] pt-8 lg:pt-[72px] lg:w-[1512px] lg:h-[710px] justify-center lg:gap-20 lg:items-start items-center">
           <div className="flex flex-col items-center lg:items-start">
             <div className="text-center text-[12px] pb-[20px] lg:pt-[112px]">
               <img className="w-[380px] text-[12px] h-[106px] my-[-20px] lg:m-[-28px] font-sfPro" src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="Facebook" />
